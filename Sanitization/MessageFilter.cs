b3be5f3417aa83e00a82141e0e9f5c82c25cd7d9
GIRU <girugiru100x@gmail.com>
2021-08-17 22:05:18 +0100

    updated message filter to support regex

--- Sanitization/MessageFilter.cs
+++ Sanitization/MessageFilter.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AdMortemBot.Logging;
+using Discord;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +15,37 @@ namespace AdMortemBot.Sanitization
             "stupid",
             "idiot",
         };
+
+       
+
+
+        public async static Task<bool> CheckToFilterMessage(string messageToFilter)
+        {
+            try
+            {
+                return await CheckMatchesFilterRegex(messageToFilter) || await CheckProfanity(messageToFilter);
+            }
+            catch (Exception ex)
+            {
+                await Logger.LogMessageAsync(new LogMessage(LogSeverity.Error, $"MessageFilter: { Logger.GetAsyncMethodName()}", ex.Message, ex));
+            }
+
+            return false;
+        }
+
+
+
+        public static Task<bool> CheckMatchesFilterRegex(string messageToFilter)
+        {
+            bool matchesRegex = false;
+
+
+
+
+
+
+            return Task.FromResult(matchesRegex);
+        }
 
 
         public static Task<bool> CheckProfanity(string messageToFilter)
